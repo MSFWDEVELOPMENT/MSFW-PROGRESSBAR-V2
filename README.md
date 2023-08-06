01ddcf031fc5f3a848acfd71a0861668b46a8501
MOWGLI0001 <gourankaneja07@gmail.com>
2023-08-06 22:42:14 +0530

    added script

---
+++ README.md
@@ -0,0 +1,18 @@
+# GD-PROGRESSBAR-V2
+
+## steps to install GD-PROGRESSBAR-V2
+
+ # step 1 - drag and drop this script in your resources/standalone folder and delete the old progressbar
+
+ # step 2 - restart your server
+ 
+ # PREVIEW
+ * https://cdn.discordapp.com/attachments/1000444357196922962/1020733727321641110/unknown.png
+
+<p><a href="https://discord.gg/V6PD44awSR">GodsDevelopment(DISCORD)</a></p>
+
+# Enjoy!
+
+# credits -
+
+# GODS DEVELOPMENT ❤️
---
+++ progressbar/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GodsDevelopment'
+description 'GD-PROGRESSBAR-V2'
+version '1.0.0'
+
+ui_page('html/index.html') 
+
+client_scripts {
+    'client/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+
+    'html/css/bootstrap.min.css',
+    'html/js/jquery.min.js',
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick'
+}
